f727e1762b52a690cbb17dc1c303a8e2dd785969
J Litt <jdlitton2594@gmail.com>
2022-06-20 21:35:42 -0500

    almost done

--- assets/js/script-two.js
+++ assets/js/script-two.js
@@ -9,6 +9,13 @@ let source = "https://embed.windy.com/embed2.html"
 let dashboard = document.querySelector("#dashboard");
 let hourBox = document.querySelector("#middle-container");
 let fiveForecast = document.querySelector("#forecast");
+let historyBar = document.querySelector("#history")
+
+let city = JSON.parse(localStorage.getItem("cities"))
+
+let currentWeather = {};
+let hourWeather = {};
+let fiveDayForecast = {};
 
 function citySearch () {
     const options = {
@@ -19,7 +26,7 @@ function citySearch () {
         }
     };
     
-    fetch('https://community-open-weather-map.p.rapidapi.com/weather?q=' + $('#new-city').val() + '&lang=null&units=imperial', options)
+    fetch('https://community-open-weather-map.p.rapidapi.com/weather?q=' + JSON.parse(city[0]) + '&lang=null&units=imperial', options)
     .then((response) => {
         if (response.ok) {
           return response.json();
@@ -29,11 +36,10 @@ function citySearch () {
       })
       .then(data => {
         console.log(data);
-        localStorage.setItem("city data", JSON.stringify(data))
+        currentWeather = data;
       })
       .catch((error) => console.error("FETCH ERROR:", error));
 
-// hourlyWeather();
 };
 
 function hourlyWeather () {
@@ -45,7 +51,7 @@ function hourlyWeather () {
     }
   };
   
-  fetch('https://community-open-weather-map.p.rapidapi.com/forecast?q=' + $('#new-city').val() + '%2Cus&units=imperial', options)
+  fetch('https://community-open-weather-map.p.rapidapi.com/forecast?q=' + JSON.parse(city[0]) + '%2Cus&units=imperial', options)
   .then((response) => {
     if (response.ok) {
       return response.json();
@@ -55,11 +61,10 @@ function hourlyWeather () {
   })
   .then(data => {
     console.log(data);
-    localStorage.setItem("hourly", JSON.stringify(data))
+    hourWeather = data;
   })
   .catch((error) => console.error("FETCH ERROR:", error));
 
-//   fiveDay();
 };
 
 function fiveDay () {
@@ -71,7 +76,7 @@ function fiveDay () {
     }
   };
   
-  fetch('https://community-open-weather-map.p.rapidapi.com/forecast/daily?q=' + $('#new-city').val() + '&cnt=6&units=imperial', options)
+  fetch('https://community-open-weather-map.p.rapidapi.com/forecast/daily?q=' + JSON.parse(city[0]) + '&cnt=6&units=imperial', options)
   .then((response) => {
     if (response.ok) {
       return response.json();
@@ -81,61 +86,55 @@ function fiveDay () {
   })
   .then(data => {
     console.log(data);
-    localStorage.setItem("five day", JSON.stringify(data))
+    fiveDayForecast = data;
   })
   .catch((error) => console.error("FETCH ERROR:", error));
 
-//   setTimeout(populatePage, 1000)
+  setTimeout(populatePage, 1000)
 };
 
 function populatePage() {
-    let cityData = JSON.parse(localStorage.getItem("city data"));
-    console.log(cityData);
-
     let cityName = document.querySelector("#city-name");
-    cityName.textContent = cityData.name;
+    cityName.textContent = currentWeather.name;
 
     let currentWea = document.querySelector("#current-weather");
     let conditions = document.createElement("div");
-    conditions.innerHTML = "<img src='http://openweathermap.org/img/wn/" + cityData.weather[0].icon + ".png' width='100' height='100'><br><h4>" + cityData.weather[0].main + "</h4>";
+    conditions.innerHTML = "<img src='http://openweathermap.org/img/wn/" + currentWeather.weather[0].icon + ".png' width='100' height='100'><br><h4>" + currentWeather.weather[0].main + "</h4>";
     currentWea.appendChild(conditions);
 
 
     let currentTemp = document.querySelector("#current-temp");
-    currentTemp.textContent = Math.round(cityData.main.temp) + "° F";
+    currentTemp.textContent = Math.round(currentWeather.main.temp) + "° F";
 
     let currentFeel = document.querySelector("#feels-like");
-    currentFeel.textContent = Math.round(cityData.main.feels_like) + "° F";
+    currentFeel.textContent = Math.round(currentWeather.main.feels_like) + "° F";
 
     let clouds = document.querySelector("#clouds");
-    clouds.textContent = cityData.clouds.all + " %";
+    clouds.textContent = currentWeather.clouds.all + " %";
 
     let currPress = document.querySelector("#pressure");
-    currPress.textContent = cityData.main.pressure + " hPa";
+    currPress.textContent = currentWeather.main.pressure + " hPa";
 
     let currHumid = document.querySelector("#humidity");
-    currHumid.textContent = cityData.main.humidity + "%";
+    currHumid.textContent = currentWeather.main.humidity + "%";
 
     let currWind = document.querySelector("#wind");
-    currWind.textContent = cityData.wind.deg + "°, " + Math.round(cityData.wind.speed) + " mph";
+    currWind.textContent = currentWeather.wind.deg + "°, " + Math.round(currentWeather.wind.speed) + " mph";
 
     let radarMap = document.querySelector("#radar-map");
-    radarMap.innerHTML = "<iframe src='" + source + "?lat=" + cityData.coord.lat + "&lon=" + cityData.coord.lon + "&detailLat=" + cityData.coord.lat + "&detailLon=" + cityData.coord.lon + "&width=650&height=450&zoom=8&level=surface&overlay=radar&product=radar&menu=&message=&marker=&calendar=now&pressure=&type=map&location=coordinates&detail=&metricWind=default&metricTemp=default&radarRange=-1' frameborder='0'></iframe>"
+    radarMap.innerHTML = "<iframe src='" + source + "?lat=" + currentWeather.coord.lat + "&lon=" + currentWeather.coord.lon + "&detailLat=" + currentWeather.coord.lat + "&detailLon=" + currentWeather.coord.lon + "&width=650&height=450&zoom=8&level=surface&overlay=radar&product=radar&menu=&message=&marker=&calendar=now&pressure=&type=map&location=coordinates&detail=&metricWind=default&metricTemp=default&radarRange=-1' frameborder='0'></iframe>"
 
     loadHourly();
     forecastFive();
 }
 
 function loadHourly () {
-    let hourly = JSON.parse(localStorage.getItem("hourly"));
-    console.log(hourly);
-
-    for (i = 0; i < 7; i++) {
+  for (i = 0; i < 7; i++) {
     let hourLine = document.createElement("div");
     hourLine.className = "hour-line variables"
     let time = document.createElement("p")
 
-    let date = new Date (hourly.list[i].dt * 1000);
+    let date = new Date (hourWeather.list[i].dt * 1000);
 
     function getTwelve(date) {
         return date.getHours() % 12 || 12;
@@ -145,46 +144,44 @@ function loadHourly () {
     time.textContent = getTwelve(date) + ":" + minutes + "0 ";
 
     let conditions = document.createElement("p");
-    conditions.innerHTML= "<img src='http://openweathermap.org/img/wn/" + hourly.list[i].weather[0].icon + ".png'><br><p>" + hourly.list[i].weather[0].main + "</p>"
+    conditions.innerHTML= "<img src='http://openweathermap.org/img/wn/" + hourWeather.list[i].weather[0].icon + ".png'><br><p>" + hourWeather.list[i].weather[0].main + "</p>"
 
     let temperature = document.createElement("h5");
-    temperature.textContent = Math.round(hourly.list[i].main.temp) + "°"
+    temperature.textContent = Math.round(hourWeather.list[i].main.temp) + "°"
     hourLine.append(time, conditions, temperature)
     hourBox.appendChild(hourLine);
-    };
+  };
 }
 
 function forecastFive () {
-    let forecast = JSON.parse(localStorage.getItem("five day"));
-    console.log(forecast);
+  for (i = 1; i < fiveDayForecast.list.length; i++) {
+    let fiveFore = document.createElement("div")
+    fiveFore.className = "card col text-bg-light vh-50 vw-10 five-day border-info"
 
-    for (i = 1; i < forecast.list.length; i++) {
-        let fiveFore = document.createElement("div")
-        fiveFore.className = "card col text-bg-light vh-50 vw-10 five-day border-info"
+    let fiveCond = document.createElement("div")
+    fiveCond.className = "condition-container"
 
-        let fiveCond = document.createElement("div")
-        fiveCond.className = "condition-container"
-
-        let foreDay = document.createElement("h2");
-        foreDay.className = "variables"
-        let date = new Date (forecast.list[i].dt *1000);
-        const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-        let name = month[date.getMonth()];
-        let day = date.getDate();
-        // document.write(day.toDateString());
-        foreDay.textContent = (name) + " " + day
+    let foreDay = document.createElement("h2");
+    foreDay.className = "variables"
+    let date = new Date (fiveDayForecast.list[i].dt *1000);
+    const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+    let name = month[date.getMonth()];
+    let day = date.getDate();
+    
+    foreDay.textContent = (name) + " " + day
         
-        let fiveWea = document.createElement("div");
-        fiveWea.innerHTML = "<img src='http://openweathermap.org/img/wn/" + forecast.list[i].weather[0].icon + ".png' width='100' height='100'><br><h3>" + forecast.list[i].weather[0].main + "</h3>"
+    let fiveWea = document.createElement("div");
+    fiveWea.innerHTML = "<img src='http://openweathermap.org/img/wn/" + fiveDayForecast.list[i].weather[0].icon + ".png' width='100' height='100'><br><h3>" + fiveDayForecast.list[i].weather[0].main + "</h3>"
 
-        let fiveTemp = document.createElement("div");
-        fiveTemp.innerHTML = "<h1>" + Math.round(forecast.list[i].temp.max) + "° F</h1><br><h4>" + Math.round(forecast.list[i].temp.min) + "° F</h4>"
+    let fiveTemp = document.createElement("div");
+    fiveTemp.innerHTML = "<h1>" + Math.round(fiveDayForecast.list[i].temp.max) + "° F</h1><br><h4>" + Math.round(fiveDayForecast.list[i].temp.min) + "° F</h4>"
         
-        fiveCond.append(fiveWea, fiveTemp)
-        fiveFore.append(foreDay, fiveCond)
-        fiveForecast.appendChild(fiveFore);
-        // console.log(forecast.list[0].)
-    }
+    fiveCond.append(fiveWea, fiveTemp)
+    fiveFore.append(foreDay, fiveCond)
+    fiveForecast.appendChild(fiveFore);
+    };
+
+ 
 }
 
 function currentTime () {
@@ -194,14 +191,49 @@ function currentTime () {
 }
 
 function searchAgain(event) {
-    event.preventDefault();
-    citySearch();
-    fiveDay();
-    hourlyWeather();
-
-    setTimeout(location.reload(), 5000);
+  event.preventDefault();
+  newCity = JSON.stringify($('#new-city').val())
+  city.unshift(newCity);
+  localStorage.setItem("cities", JSON.stringify(city))
+  let reload = location.reload()
+  setTimeout(reload, 5000);
 }
 
-populatePage();
+
+
+function populateHistory () {
+  for (let i = 0; i < city.length; i++) {
+    let history = document.createElement("div")
+    let prevCity = document.createElement("button")
+
+    prevCity.id = city[i]
+    prevCity.value = city[i]
+    prevCity.className = "city-history btn btn-primary" 
+    prevCity.textContent = JSON.parse(city[i]);
+    history.appendChild(prevCity)
+    historyBar.appendChild(history);
+    
+    document.getElementById(city[i]).addEventListener('click', reloadPlace)
+
+    function reloadPlace(event) {
+    event.preventDefault();
+    let location = document.getElementById(city[i]).value
+    city.unshift(location);
+    localStorage.setItem("cities", JSON.stringify(city))
+    reloadPage();
+    };
+  };
+};
+
+function reloadPage () {
+  let reloadTwo = location.reload();
+  setTimeout(reloadTwo, 5000)
+}
+
+citySearch();
+fiveDay();
+hourlyWeather();
+
+setTimeout(populateHistory, 3000)
 setInterval(currentTime, 1000);
 $("#search-again").on('click', searchAgain);
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,117 +1,20 @@
 var key = config.MY_API_KEY;
 
+const cities = [];
+
 function nextPage () {
-    window.location.href = "index-two.html"
+  window.location.href = "index-two.html"
 }
 
-function search (event) {
+function saveCity (event) {
   event.preventDefault();
-  var city = {};
-
-  function saveData(weatherData) {
-    var weatherData = [];
-    weatherData.push(city);
-    console.log(weatherData);
-    localStorage.setItem("weather", JSON.stringify(weatherData));
-  };
-
-  // function pushData () {
-      
-  //     
-  //     saveData();
-  // };
-
-  function citySearch () {
-    const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': key,
-            'X-RapidAPI-Host': 'community-open-weather-map.p.rapidapi.com'
-        }
-    };
-    
-    fetch('https://community-open-weather-map.p.rapidapi.com/weather?q=' + $('#city-search').val() + '&lang=null&units=imperial', options)
-    .then((response) => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          throw new Error("Sorry, we were unable to complete your request.");
-        }
-      })
-      .then(data => {
-        console.log(data);
-        let current = data
-        city.current = JSON.stringify(current);
-        // localStorage.setItem("city data", weatherData.current)
-      })
-      .catch((error) => console.error("FETCH ERROR:", error));
-
-  };
-
-  function hourlyWeather () {
-  const options = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': key,
-      'X-RapidAPI-Host': 'community-open-weather-map.p.rapidapi.com'
-    }
-  };
-  
-  fetch('https://community-open-weather-map.p.rapidapi.com/forecast?q=' + $('#city-search').val() + '%2Cus&units=imperial', options)
-  .then((response) => {
-    if (response.ok) {
-      return response.json();
-    } else {
-      throw new Error("Sorry, we were unable to complete your request.");
-    }
-  })
-  .then(data => {
-    console.log(data);
-    let hourly = data
-    city.hourly = JSON.stringify(hourly);
-    // localStorage.setItem("hourly", )
-  })
-  .catch((error) => console.error("FETCH ERROR:", error));
-
-};
-
-function fiveDay () {
-  const options = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': key,
-      'X-RapidAPI-Host': 'community-open-weather-map.p.rapidapi.com'
-    }
-  };
-  
-  fetch('https://community-open-weather-map.p.rapidapi.com/forecast/daily?q=' + $('#city-search').val() + '&cnt=6&units=imperial', options)
-  .then((response) => {
-    if (response.ok) {
-      return response.json();
-    } else {
-      throw new Error("Sorry, we were unable to complete your request.");
-    }
-  })
-  .then(data => {
-    console.log(data);
-    let forecast = data
-    city.forecast = JSON.stringify(forecast);
-    // localStorage.setItem("five day", )
-  })
-  .catch((error) => console.error("FETCH ERROR:", error));
-
-  // pushData();
-  saveData();
+  cityName = $('#city-search').val()
+  cities.push(JSON.stringify(cityName));
+  localStorage.setItem("cities", JSON.stringify(cities))
+  console.log(cities)
+  setTimeout(nextPage, 2000)
 };
 
 
 
-citySearch();
-hourlyWeather();
-fiveDay();
-
-};
-
-
-
-$('#start').on('click', search);
+$('#start').on('click', saveCity);
